6208887ad4273d8c98b3753da8d6385567b23250
DoganHaluk <ha_look@yahoo.com>
2021-02-20 22:17:05 +0100

    Test for Maven

---
+++ src/test/java/be/vdab/domain/ConverterTest.java
@@ -0,0 +1,21 @@
+package be.vdab.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ConverterTest {
+    @Test
+    void eenInchIs2Komma54Centimeters() {
+        assertThat(new Converter().inchesNaarCentimeters(BigDecimal.ONE))
+                .isEqualByComparingTo("2.54");
+    }
+
+    @Test
+    void tweehonderdInchesIs508Centimeters() {
+        assertThat(new Converter().inchesNaarCentimeters(BigDecimal.valueOf(200)))
+                .isEqualByComparingTo("508");
+    }
+}
