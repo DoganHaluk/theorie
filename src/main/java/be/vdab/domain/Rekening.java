2c94e1aa45f80b7c594008fab55fd3dec42a9ddd
DoganHaluk <ha_look@yahoo.com>
2021-02-20 22:54:06 +0100

    Test for Maven

---
+++ src/main/java/be/vdab/domain/Rekening.java
@@ -0,0 +1,13 @@
+package be.vdab.domain;
+
+import java.math.BigDecimal;
+
+public class Rekening {
+    private BigDecimal saldo = BigDecimal.valueOf(0);
+    public void stort(BigDecimal bedrag) {
+        saldo = saldo.add(bedrag);
+    }
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+}
