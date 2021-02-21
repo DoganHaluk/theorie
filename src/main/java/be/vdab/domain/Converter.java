b1ea49fd2c5ebca0f8901edb8485e0cfd1837bcb
DoganHaluk <ha_look@yahoo.com>
2021-02-21 03:58:59 +0100

    Test and Maven Theory

--- src/main/java/be/vdab/domain/Converter.java
+++ src/main/java/be/vdab/domain/Converter.java
@@ -3,8 +3,8 @@ package be.vdab.domain;
 import java.math.BigDecimal;
 
 public class Converter {
-    private static final BigDecimal AANTAL_CENTIMETER_IN_EEN_INCH
-            = BigDecimal.valueOf(2.54);
+    private static final BigDecimal AANTAL_CENTIMETER_IN_EEN_INCH = BigDecimal.valueOf(2.54);
+
     public BigDecimal inchesNaarCentimeters(BigDecimal inches) {
         return inches.multiply(AANTAL_CENTIMETER_IN_EEN_INCH);
     }
--- src/test/java/be/vdab/domain/ConverterTest.java
+++ src/test/java/be/vdab/domain/ConverterTest.java
@@ -9,13 +9,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ConverterTest {
     @Test
     void eenInchIs2Komma54Centimeters() {
-        assertThat(new Converter().inchesNaarCentimeters(BigDecimal.ONE))
-                .isEqualByComparingTo("2.54");
+        assertThat(new Converter().inchesNaarCentimeters(BigDecimal.ONE)).isEqualByComparingTo("2.54");
     }
 
     @Test
     void tweehonderdInchesIs508Centimeters() {
-        assertThat(new Converter().inchesNaarCentimeters(BigDecimal.valueOf(200)))
-                .isEqualByComparingTo("508");
+        assertThat(new Converter().inchesNaarCentimeters(BigDecimal.valueOf(200))).isEqualByComparingTo("508");
     }
 }
